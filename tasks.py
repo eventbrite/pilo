0985d32996e4f1c71c9daec5619d45c2187c5ff0
zordon-team <zordon@eventbrite.com>
2021-07-12 16:33:11 -0300

    Removed CircleCI

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke_release.tasks import *  # noqa: F403
-
-
-configure_release_parameters(  # noqa: F405
-    module_name='pilo',
-    display_name='Pilo',
-    use_pull_request=True,
-    use_tag=False,
-)
